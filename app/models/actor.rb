daf9bb4859cebb7a23e14415ba66a61099ef8bd1
Nick Su <hsu0@chicagobooth.edu>
2017-08-01 05:07:53 +0000

    test 7

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -17,6 +17,7 @@ validates :name, :presence => true, :uniqueness => {:scope => :dob}
 #  - dob: no rules
 #  - bio: no rules
 #  - image_url: no rules
-has_many :movies
 has_many :characters
+has_many :movies, :through => :characters
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -21,9 +21,11 @@ validates :title, :presence => true, :uniqueness => { :scope => :year}
 #  - year: must be integer between 1870 and 2050
 validates :year, :numericality => {:only_integer => true, :greater_than_or_equal_to => 1870, :less_than_or_equal_to =>2050}
 #  - duration: must be integer between 0 and 2764800, but it can be blank (hint: there is an option `:allow_blank => true`)
-validates :duration, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to =>2764800}
+# validates :duration, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to =>2764800}
 
 belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
-has_many :actors
+
 has_many :characters
+has_many :actors, :through => :characters
+
 end
